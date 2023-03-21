0d75d7bd1ef3f794881f980ccfedc9e47c0f6c24
Jorik-Laptop <mastakcompany@gmail.com>
2023-03-21 18:04:04 +0700

    Publish photo to Telegram channel

--- main.py
+++ main.py
@@ -4,11 +4,17 @@ import telegram
 from dotenv import load_dotenv
 
 
-def sent_message(token, chat_id):
-    bot = telegram.Bot(token=token)
+def sent_message(bot, chat_id):
     bot.send_message(
         text='Hi! This is the test message.',
         chat_id=chat_id
+    )
+
+
+def send_photo(bot, chat_id):
+    bot.send_photo(
+        chat_id=chat_id,
+        photo=open('./images/epic_1b_20230319152855.jpg', 'rb')
     )
 
 
@@ -16,4 +22,6 @@ if __name__ == '__main__':
     load_dotenv()
     token = os.environ['TELEGRAM_TOKEN']
     chat_id = os.environ['TELEGRAM_CHANNEL_ID']
-    sent_message(token, chat_id)
+    bot = telegram.Bot(token=token)
+    send_photo(bot, chat_id)
+
